fc59a46895600cfdced780839d6c8b4dda60020a
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2021-09-03 17:14:47 +0100

    add test utils and a test for the baseline delay and sum implementation

--- tests/reconstruction_algorithms/__init__.py
+++ tests/reconstruction_algorithms/__init__.py
@@ -6,6 +6,9 @@ SPDX-License-Identifier: MIT
 import unittest
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
 
 
 class TestClassBase(unittest.TestCase):
@@ -25,14 +28,50 @@ class TestClassBase(unittest.TestCase):
         if not os.path.exists(self.file_name):
             raise FileNotFoundError(f"Could not find {self.file_name} in the test package and could not"
                                     f"download automatically from google drive. In order to run the tests,"
-                                    f"please manually download the file from {download_url}, name it {self.file_name},"
+                                    f"please manually download the file from "
+                                    f"https://drive.google.com/file/d/1RzzNrwGewkyX8be"
+                                    f"Voi7zHiQ4BxdUkbRQ/view?usp=sharing, name it {self.file_name},"
+                                    f"and place it into the 'tests/reconstruction_algorithms' folder.")
+
+    def download_expected_result_file(self):
+        download_url = "1l_9QTqCcIqW58d_YHPcTixUyB0OrSkq5"
+
+        if not os.path.exists(self.file_name_result):
+            gdd.download_file_from_google_drive(file_id=download_url, dest_path=self.result_file_path)
+        else:
+            print(f"File {self.file_name_result} already in folder. Did not re-download")
+
+        if not os.path.exists(self.file_name_result):
+            raise FileNotFoundError(f"Could not find {self.file_name_result} in the test package and could not"
+                                    f"download automatically from google drive. In order to run the tests,"
+                                    f"please manually download the file from "
+                                    f"https://drive.google.com/file/d/1l_9QTqCcIqW58d_"
+                                    f"YHPcTixUyB0OrSkq5/view?usp=sharing, name it {self.file_name_result},"
                                     f"and place it into the 'tests/reconstruction_algorithms' folder.")
 
     def __init__(self, arg):
         super(TestClassBase, self).__init__(arg)
         self.file_name = "sample_file.hdf5"
+        self.file_name_result = "sample_result.png"
         self.ipasc_hdf5_file_path = os.path.abspath("./"+self.file_name)
+        self.result_file_path = os.path.abspath("./" + self.file_name_result)
         self.download_sample_ipasc_file()
+        self.download_expected_result_file()
 
     def setUp(self) -> None:
         self.download_sample_ipasc_file()
+
+    def visualise_result(self, result: np.ndarray):
+        expected_result = imageio.imread(self.result_file_path)
+        plt.figure(figsize=(8, 3))
+        plt.subplot(1, 2, 1)
+        plt.title("Reference Reconstruction")
+        plt.axis("off")
+        plt.imshow(expected_result)
+        plt.subplot(1, 2, 2)
+        plt.title("Reconstruction Result")
+        plt.imshow(np.rot90(result[:, 0, :, 0, 0], -1))
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
+        plt.close()
---
+++ tests/reconstruction_algorithms/test_baseline_delay_and_sum.py
@@ -0,0 +1,21 @@
+"""
+SPDX-FileCopyrightText: 2021 International Photoacoustic Standardisation Consortium (IPASC)
+SPDX-License-Identifier: MIT
+"""
+
+from tests.reconstruction_algorithms import TestClassBase
+from image_reconstruction.baseline_delay_and_sum import BaselineDelayAndSumReconstruction
+
+
+class TestDelayAndSum(TestClassBase):
+
+    def test_delay_and_sum_reconstruction_is_running_through(self):
+        reco = BaselineDelayAndSumReconstruction()
+        result = reco.reconstruct_time_series_data(self.ipasc_hdf5_file_path, **{
+            "spacing_m": 0.0001,
+            "speed_of_sound_m_s": 1540
+        })
+        self.visualise_result(result)
+
+
+
