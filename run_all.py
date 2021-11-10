fd9ad8853a586d37788dad5a6173f1fef91994f5
varray <francois.varray@creatis.insa-lyon.fr>
2021-11-10 16:48:12 +0100

    add the pDAS beamforming in the branch

--- run_all.py
+++ run_all.py
@@ -1,7 +1,8 @@
 from tests.reconstruction_algorithms.test_baseline_delay_and_sum import TestDelayAndSum
 import matplotlib.pyplot as plt
 
-out = TestDelayAndSum("DAS")
+out = TestDelayAndSum("TestDelayAndSum")
 out.test_delay_and_sum_reconstruction_is_running_through()
 out.test_delay_and_sum_reconstruction_is_running_through_fnumber()
+out.test_delay_and_sum_reconstruction_is_running_through_pDAS()
 plt.show()
--- tests/reconstruction_algorithms/test_baseline_delay_and_sum.py
+++ tests/reconstruction_algorithms/test_baseline_delay_and_sum.py
@@ -6,6 +6,7 @@ SPDX-License-Identifier: MIT
 from tests.reconstruction_algorithms import TestClassBase
 from image_reconstruction.reconstruction_algorithms.baseline_delay_and_sum import BaselineDelayAndSumAlgorithm
 from image_reconstruction.reconstruction_algorithms.baseline_delay_and_sum_fnumber import BaselineDelayAndSumAlgorithmFnumber
+from image_reconstruction.reconstruction_algorithms.baseline_delay_and_sum_pDAS import BaselineDelayAndSumAlgorithmpDAS
 
 
 class TestDelayAndSum(TestClassBase):
@@ -21,6 +22,14 @@ class TestDelayAndSum(TestClassBase):
 
     def test_delay_and_sum_reconstruction_is_running_through_fnumber(self):
         reco = BaselineDelayAndSumAlgorithmFnumber()
+        result = reco.reconstruct_time_series_data(self.ipasc_hdf5_file_path, **{
+            "spacing_m": 0.0001,
+            "speed_of_sound_m_s": 1540
+        })
+        self.visualise_result(result)
+
+    def test_delay_and_sum_reconstruction_is_running_through_pDAS(self):
+        reco = BaselineDelayAndSumAlgorithmpDAS()
         result = reco.reconstruct_time_series_data(self.ipasc_hdf5_file_path, **{
             "spacing_m": 0.0001,
             "speed_of_sound_m_s": 1540
