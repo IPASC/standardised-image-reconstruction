df9ae2e83f5365c4eeb751c57019d81f74c1be9c
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2021-11-16 13:20:22 +0100

    First sDMAS version

---
+++ tests/reconstruction_algorithms/test_signed_delay_multiply_and_sum.py
@@ -0,0 +1,19 @@
+"""
+SPDX-FileCopyrightText: 2021 International Photoacoustic Standardisation Consortium (IPASC)
+SPDX-License-Identifier: MIT
+"""
+
+from tests.reconstruction_algorithms import TestClassBase
+from image_reconstruction.reconstruction_algorithms.signed_delay_multiply_and_sum import \
+    SignedDelayMultiplyAndSumAlgorithm
+
+
+class TestDelayMultiplyAndSum(TestClassBase):
+
+    def test_delay_multiply_and_sum_reconstruction_is_running_through(self):
+        reco = SignedDelayMultiplyAndSumAlgorithm()
+        result = reco.reconstruct_time_series_data(self.ipasc_hdf5_file_path, **{
+            "spacing_m": 0.0001,
+            "speed_of_sound_m_s": 1540
+        })
+        self.visualise_result(result)
