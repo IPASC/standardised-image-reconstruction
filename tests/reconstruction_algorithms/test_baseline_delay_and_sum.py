5eda156f11e35996e4f67b1bf0f016d93174409f
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2021-11-23 16:32:54 +0000

    rewrote test and added more example data

--- tests/reconstruction_algorithms/test_baseline_delay_and_sum.py
+++ tests/reconstruction_algorithms/test_baseline_delay_and_sum.py
@@ -1,5 +1,6 @@
 """
 SPDX-FileCopyrightText: 2021 International Photoacoustic Standardisation Consortium (IPASC)
+SPDX-FileCopyrightText: 2021 Janek Groehl
 SPDX-License-Identifier: MIT
 """
 
@@ -10,12 +11,10 @@ from image_reconstruction.reconstruction_algorithms.baseline_delay_and_sum impor
 class TestDelayAndSum(TestClassBase):
 
     def test_delay_and_sum_reconstruction_is_running_through(self):
-        reco = BaselineDelayAndSumAlgorithm()
-        result = reco.reconstruct_time_series_data(self.ipasc_hdf5_file_path, **{
+        self.run_tests(BaselineDelayAndSumAlgorithm(), **{
             "spacing_m": 0.0001,
             "speed_of_sound_m_s": 1540
         })
-        self.visualise_result(result)
 
 
 
