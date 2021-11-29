correct formulation for SCF calculation

image_reconstruction/reconstruction_algorithms/baseline_delay_and_sum_SCF.py
@@ -17,6 +17,7 @@ from image_reconstruction.reconstruction_algorithms import ReconstructionAlgorit
 class BaselineDelayAndSumAlgorithmSCF(ReconstructionAlgorithm):
 
     p_factor = 1
+    p_SCF = 1
     fnumber = 0
 
     def implementation(self, time_series_data: np.ndarray,
@@ -52,6 +53,9 @@ class BaselineDelayAndSumAlgorithmSCF(ReconstructionAlgorithm):
 
         if "p_factor" in kwargs:
             self.p_factor = kwargs["p_factor"]
+
+        if "p_SCF" in kwargs:
+            self.p_SCF = kwargs["p_SCF"]
 
         if "fnumber" in kwargs:
             self.fnumber = kwargs["fnumber"]
@@ -89,7 +93,8 @@ class BaselineDelayAndSumAlgorithmSCF(ReconstructionAlgorithm):
 
 
         # We extract and sum the sign of the value
-        _SCF = torch.sum(torch.sign(values), dim=3)
+        _SCF = torch.mean(torch.sign(values), dim=3)
+        _SCF = torch.pow(torch.abs(1-torch.sqrt(1-torch.pow(_SCF, 2))), self.p_SCF)
 
         # We do sign(s)*abs(s)^(1/p)
         values = torch.mul( torch.sign(values), torch.pow(torch.abs(values), 1/self.p_factor))
@@ -102,7 +107,8 @@ class BaselineDelayAndSumAlgorithmSCF(ReconstructionAlgorithm):
         counter = torch.count_nonzero(values, dim=3)
 
         # We divide by the number of nonzeros value the sum of the SCF of before (mean calculation)
-        _SCF = torch.divide(_SCF, counter)
+        #_SCF = torch.divide(_SCF, counter)
+
         # We multiply with the SCF coeeficient
         _sum = torch.mul(_sum, _SCF)
 