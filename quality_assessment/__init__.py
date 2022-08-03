539e1bdb36cd7cb72e9048ccafdd0942d5697ea8
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2022-08-03 12:00:43 +0100

    add documentation and license information

---
+++ quality_assessment/__init__.py
@@ -0,0 +1,4 @@
+"""
+SPDX-FileCopyrightText: 2021 International Photoacoustic Standardisation Consortium (IPASC)
+SPDX-License-Identifier: MIT
+"""
--- quality_assessment/measures/full_reference.py
+++ quality_assessment/measures/full_reference.py
@@ -1,3 +1,10 @@
+"""
+SPDX-FileCopyrightText: 2021 International Photoacoustic Standardisation Consortium (IPASC)
+SPDX-FileCopyrightText: 2022 Janek Grohl
+SPDX-FileCopyrightText: 2022 Lina Hacker
+SPDX-License-Identifier: MIT
+"""
+
 from quality_assessment.measures import FullReferenceMeasure
 from sewar import ssim, uqi, rmse
 from sklearn.metrics import mutual_info_score
@@ -8,20 +15,32 @@ class StructuralSimilarityIndex(FullReferenceMeasure):
     def compute_measure(self, ground_truth_image, reconstructed_image):
         return ssim(ground_truth_image, reconstructed_image, ws=8)[0]
 
+    def get_name(self):
+        return "SSIM"
+
 
 class UniversalQualityIndex(FullReferenceMeasure):
 
     def compute_measure(self, expected_result, reconstructed_image):
         return uqi(expected_result, reconstructed_image, ws=8)
 
+    def get_name(self):
+        return "UQI"
 
-class MeanSquaredError(FullReferenceMeasure):
+
+class RootMeanSquaredError(FullReferenceMeasure):
 
     def compute_measure(self, expected_result, reconstructed_image):
         return rmse(expected_result, reconstructed_image)
+
+    def get_name(self):
+        return "RMSE"
 
 
 class MutualInformation(FullReferenceMeasure):
 
     def compute_measure(self, expected_result, reconstructed_image):
         return mutual_info_score(expected_result.reshape((-1, )), reconstructed_image.reshape((-1, )))
+
+    def get_name(self):
+        return "MI"
