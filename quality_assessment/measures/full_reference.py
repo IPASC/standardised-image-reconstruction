2ed6b4e76bda33d62b27c48c41abc19c45c32ff1
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2023-07-07 09:43:51 +0100

    changes to the quality measures

--- quality_assessment/measures/full_reference.py
+++ quality_assessment/measures/full_reference.py
@@ -5,19 +5,17 @@ SPDX-FileCopyrightText: 2022 Lina Hacker
 SPDX-FileCopyrightText: 2022 Shufan Yang
 SPDX-License-Identifier: MIT
 """
-import torch
+
 import torchvision.transforms as transforms
-import numpy as np
-from PIL import Image
 from quality_assessment.measures import FullReferenceMeasure
-from sewar import ssim, uqi, rmse
+from sewar import rmse
 from sklearn.metrics import mutual_info_score
+from torchmetrics import UniversalImageQualityIndex
 from torchmetrics import StructuralSimilarityIndexMeasure
-from torchmetrics import StructuralSimilarityIndexMeasure
 
 
 
-class StructuralSimilarityIndexTorch(FullReferenceMeasure):
+class StructuralSimilarityIndex(FullReferenceMeasure):
 
     def compute_measure(self, ground_truth_image, reconstructed_image):
 
@@ -25,24 +23,18 @@ class StructuralSimilarityIndexTorch(FullReferenceMeasure):
         reconstructed_tensor = transform(reconstructed_image)
         ground_tensor = transform(ground_truth_image)
         ssimtorch = StructuralSimilarityIndexMeasure()
-        return ssimtorch (reconstructed_tensor,ground_tensor)
-
-    def get_name(self):
-        return "SSIMtorch"
-
-class StructuralSimilarityIndex(FullReferenceMeasure):
-
-    def compute_measure(self, ground_truth_image, reconstructed_image):
-        return ssim(ground_truth_image, reconstructed_image, ws=8, MAX=np.max([np.max(ground_truth_image), np.max(reconstructed_image)]))[0]
+        return ssimtorch(reconstructed_tensor, ground_tensor)
 
     def get_name(self):
         return "SSIM"
 
-
 class UniversalQualityIndex(FullReferenceMeasure):
 
     def compute_measure(self, expected_result, reconstructed_image):
-        return uqi(expected_result, reconstructed_image, ws=8)
+        transform = transforms.Compose([transforms.PILToTensor()])
+        reconstructed_tensor = transform(reconstructed_image)
+        ground_tensor = transform(expected_result)
+        return UniversalImageQualityIndex()(reconstructed_tensor, ground_tensor)
 
     def get_name(self):
         return "UQI"
